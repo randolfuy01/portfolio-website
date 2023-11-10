da9562bf99c60b0cf21ddfff98770e010cfdc74c
Randolf Uy <920265148@sfsu.edu>
2023-11-10 00:05:44 -0800

    created navigation bar, working on transitioning layout to grid based

---
+++ src/components/about-components/Description.js
@@ -0,0 +1,8 @@
+export const Introduction = () => {
+    return (
+        <div className="Introduction">
+            <h1> Hi my name is Randolf </h1>
+            <h1> Nice to meet you!</h1>
+        </div>
+    );
+};
---
+++ src/components/navigation-components/Navbar.js
@@ -0,0 +1,15 @@
+export const Navbar = () => {
+    return (
+        <div className="Navbar">
+            <h1 className="NavTitle"> Data + Engineering </h1>
+            <nav>
+                <ul>
+                    <li><a href="App.js"> About</a></li>
+                    <li><a href="Projects.js"> Projects</a></li>
+                    <li><a href="Blogs.js"> Blogs</a></li>
+                    <li><a href="Contact.js"> Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
